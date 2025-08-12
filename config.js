40cacf215de061fc925192e0d49aa53a208006ff
Inter-Rest <aj.bradbury23@gmail.com>
2025-08-11 17:15:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 // Paste your Notion URLs below. Leave as '#' until ready.
 // TIP: You can add query-string filtered views from Notion here too.
 window.CB_LINKS = {
-  prompts: "#",
-  outreach: "#",
-  inventory: "#",
-  seo: "#",
-  perf: "#",
+  prompts: "https://www.notion.so/Prompts-Database-24bb58038c4b80f69976cc0d47ee1850?source=copy_link",
+  outreach: "https://www.notion.so/249b58038c4b80538c12d0e0ff8e935d?v=249b58038c4b818c8d13000c3f3b54de&source=copy_link",
+  inventory: "https://www.notion.so/249b58038c4b804ea81bd8c2b005349b?v=249b58038c4b81d5ba28000cc8344855&source=copy_link",
+  seo: "https://www.notion.so/SEO-Tracker-24cb58038c4b8018aa8bdd427d1b3f94?source=copy_link",
+  perf: "https://www.notion.so/Website-Health-Checklist-24cb58038c4b80aebee8e6c425c43e5a?source=copy_link",
   sops: "#",
   automation: "#",
   events: "#",
